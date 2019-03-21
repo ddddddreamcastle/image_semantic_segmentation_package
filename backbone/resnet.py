eb6b0a6ff54d7c3971b9d7b5bb160c8d23400269
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-03-21 08:40:11 +0800

    Update resnet.py

--- backbone/resnet.py
+++ backbone/resnet.py
@@ -2,8 +2,28 @@ import torch
 import torch.utils.model_zoo as model_zoo
 import torch.nn as nn
 
+
+def conv3x3(in_channel, out_channel, stride=1):
+    return nn.Conv2d(in_channel, out_channel, 3, stride=stride, padding=1,
+                     bias=False)
+
+
+
 class ResNet(nn.Module):
 
-    def __init__(self):
+    def __init__(self, pretrained=False, nbr_classes=1000):
         super(ResNet, self).__init__()
+        self.head = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(2, 2), padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
 
