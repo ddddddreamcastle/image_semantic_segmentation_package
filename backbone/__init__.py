02309467627846ec73729ea0f1265b74c9f77b1a
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-03-22 16:38:17 +0800

    Update __init__.py

--- backbone/__init__.py
+++ backbone/__init__.py
@@ -2,6 +2,6 @@ from .resnet import get_resnet
 
 def get_backbone(name, **kwargs):
     models = {
-        'resnet': get_resnet
+        'resnet50': get_resnet(50)
     }
     return models[name](**kwargs)
