9306fcb1bc1e5ae2e67ae9eb7620b52ea22b57f1
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-03-22 16:38:25 +0800

    Update pspnet.py

--- models/pspnet.py
+++ models/pspnet.py
@@ -1,2 +1,25 @@
-def get_pspnet(backone='resnet50', pretrained=False):
-    pass
+import torch.nn as nn
+from backbone import get_backbone
+
+"""
+    Reference:
+        Zhao, Hengshuang, et al. "Pyramid scene parsing network." Proceedings of the IEEE conference on computer vision and pattern recognition. 2017.
+"""
+
+class PSPNet(nn.Module):
+    def __init__(self, nbr_classes, deep_supervision=True, backbone='resnet50'):
+        super(PSPNet, self).__init__()
+        self.backbone = get_backbone(backbone, pretrained=True)
+
+
+def get_pspnet(backone='resnet50', pretrained=True):
+    psp = PSPNet(150)
+    return psp
+
+if __name__ == '__main__':
+    psp = get_pspnet()
+    for name, f in psp.backbone.named_parameters():
+        print(name, f.data)
+        break
+
+
