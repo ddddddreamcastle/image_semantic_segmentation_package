58e7593763710593fe61414edeade5790d20f2da
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-03-25 18:20:04 +0800

    Update train.py

--- examples/train.py
+++ examples/train.py
@@ -17,23 +17,24 @@ parser.add_argument('--batch_size', type=int, default=12, help='batch size')
 # the learning rate setting refer to https://github.com/zhanghang1989/PyTorch-Encoding/blob/master/experiments/segmentation/option.py
 parser.add_argument('--learning_rate', type=float, default=None,
                     help='learning rate. If None, learning rate will be adjusted adaptively according to the dataset')
-parser.add_argument('--weight-decay', type=float, default=1e-4)
+parser.add_argument('--weight_decay', type=float, default=1e-4)
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
 parser.add_argument('--epochs', type=int, default=50, help='epochs')
 parser.add_argument('--supervision', default= True)
-parser.add_argument('--supervision-weight', type=float, default=0.4)
+parser.add_argument('--supervision_weight', type=float, default=0.4)
 
 #dataset
 parser.add_argument('--dataset', type=str, default='ade20k')
-parser.add_argument('--data-path', type=str, default='../datasets/data/ade20k')
-parser.add_argument('--image-size', type=int, default=384)
-parser.add_argument('--dataloader-workers', type=int, default=4)
+parser.add_argument('--data_path', type=str, default='../datasets/data/ade20k')
+parser.add_argument('--image_size', type=int, default=384)
+parser.add_argument('--dataloader_workers', type=int, default=4)
 
 # checkpoint and saving model
 parser.add_argument('--checkpoint_path', type=str, default='../logs/checkpoints/')
-parser.add_argument('--checkpoint-prefix-name', type=str, default='trainlog')
+parser.add_argument('--checkpoint_prefix_name', type=str, default='trainlog')
 
 args = parser.parse_args()
 manager = Manager(args)
+
 
 
