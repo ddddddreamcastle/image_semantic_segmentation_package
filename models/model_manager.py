de8904c536686b05f7d5608c234fb8715613c62b
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-03-25 18:20:06 +0800

    Update model_manager.py

--- models/model_manager.py
+++ models/model_manager.py
@@ -4,6 +4,8 @@ class Manager(object):
     def __init__(self, args):
         kwargs = vars(args)
         self.model = get_model(name=args.model, kwargs=kwargs)
+        self.train_loader = None
+        self.val_loader = None
 
     def fit(self):
         pass
