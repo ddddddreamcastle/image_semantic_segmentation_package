f6d754378b99205d97aa4965136ac5b94f7884e1
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-03-25 17:58:26 +0800

    Update train.py

--- examples/train.py
+++ examples/train.py
@@ -1,13 +1,22 @@
 import argparse
+from models.model_manager import Manager
+
 parser = argparse.ArgumentParser()
 # model and training
 parser.add_argument('--model', type=str, default='pspnet', help='model name')
+parser.add_argument('--model_pretrained', type=bool, default=False)
+parser.add_argument('--model_pretrain_path', type=str, default='../weights/pspnet_resnet50.pth',
+                    help='If `model_pretrained` is True, this param is necessary, or else will be neglected.')
 parser.add_argument('--backbone', type=str, default='resnet50', help='backbone name')
+parser.add_argument('--backbone_pretrained', type=str, default='resnet50',
+                    help='If `model_pretrained` is True, this param will be neglected.')
+parser.add_argument('--backbone_pretrained_path', type=str, default='resnet50',
+                    help='If `model_pretrained` is True, this param will be neglected.')
 parser.add_argument('--batch_size', type=int, default=12, help='batch size')
 
 # the learning rate setting refer to https://github.com/zhanghang1989/PyTorch-Encoding/blob/master/experiments/segmentation/option.py
-parser.add_argument('--learning_rate', type=float, default=None, help='learning rate. If None, learning rate will be '
-                                                                     'adjusted adaptively according to the dataset')
+parser.add_argument('--learning_rate', type=float, default=None,
+                    help='learning rate. If None, learning rate will be adjusted adaptively according to the dataset')
 parser.add_argument('--weight-decay', type=float, default=1e-4)
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
 parser.add_argument('--epochs', type=int, default=50, help='epochs')
@@ -24,20 +33,7 @@ parser.add_argument('--dataloader-workers', type=int, default=4)
 parser.add_argument('--checkpoint_path', type=str, default='../logs/checkpoints/')
 parser.add_argument('--checkpoint-prefix-name', type=str, default='trainlog')
 
+args = parser.parse_args()
+manager = Manager(args)
 
 
-
-
-
-
-# dataset
-
-arguments = {'model':'pspnet',
-             'dataset':'ade20k',
-             'backbone':'resnet50',
-             'dataloader_works':4,
-             'batch_size':12,
-             'epochs':50,
-             'learning_rate':0.003,
-             'pretrained_path':'../weights/',
-             'save_path':''}
