56fe427773ef57a1c5ff4eca5cd4d65c4cf2e8b6
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-03-26 16:31:58 +0800

    Update ade20k.py

--- datasets/ade20k.py
+++ datasets/ade20k.py
@@ -1,67 +1,83 @@
 import torch.utils.data as data
 import torchvision.transforms as transforms
 import os
+import imgaug
+from imgaug import augmenters as iaa
+import imageio
+import random
 
 class ADE20K(data.Dataset):
-    def __init__(self, mode='train', image_size=384):
+    def __init__(self, mode='train', image_size=384, data_path='./data/', dataloader_workers=4):
         self.mode = mode
-        self.mean = [0.553, 0.478, 0.364]
-        self.std = [0.262, 0.269, 0.288]
-        self.data = []
-        self.data_base_dir = os.path.join(
-            '/DATA1000/zhaopeng/workspace/research/dishes_recognition/data/chinesefoodnet',
-            'train_noise' if self.mode == 'train' else self.mode)
+        self.mean = [0.485, 0.456, 0.406]
+        self.std = [0.229, 0.224, 0.225]
+        self.data_path = data_path
+        self.workers = dataloader_workers
+        self.image_size = image_size * 1.083
+        self.crop_size = image_size
         self.init_data()
         self.im_transform = transforms.Compose([
-            transforms.Resize((image_size, image_size)),
             transforms.ToTensor(),
             transforms.Normalize(
                 mean=self.mean, std=self.std
             )
         ])
 
+    def __get_pairs(self, img_folder, mask_folder):
+        img_paths = []
+        mask_paths = []
+        for filename in os.listdir(img_folder):
+            base_filename = os.path.splitext(filename)
+            mask_filename = '{}.png'.format(base_filename)
+            if os.path.isfile(os.path.join(mask_folder, mask_filename)):
+                img_paths.append(os.path.join(img_folder, filename))
+                mask_paths.append(os.path.join(mask_folder, mask_filename))
+            else:
+                raise RuntimeError('cannot find the mask image for {}'.format(filename))
+        return img_paths, mask_paths
 
     def init_data(self):
         if self.mode == 'train':
-            datalist = pickle.load(open(
-                '/DATA1000/zhaopeng/workspace/research/dishes_recognition/data/chinesefoodnet/simple_train.pkl',
-                'rb'))
+            img_folder = os.path.join(self.data_path, 'train/images')
+            mask_folder = os.path.join(self.data_path, 'train/labels')
+            self.images, self.masks = self.__get_pairs(img_folder, mask_folder)
+        elif self.mode == 'val':
+            img_folder = os.path.join(self.data_path, 'val/images')
+            mask_folder = os.path.join(self.data_path, 'val/labels')
+            self.images, self.masks = self.__get_pairs(img_folder, mask_folder)
         else:
-            datalist = pickle.load(
-                open('/DATA1000/zhaopeng/workspace/research/dishes_recognition/data/chinesefoodnet/simple_val.pkl', 'rb'))
+            train_img_folder = os.path.join(self.data_path, 'train/images')
+            train_mask_folder = os.path.join(self.data_path, 'train/labels')
+            train_img_paths, train_mask_paths = self.__get_pairs(train_img_folder, train_mask_folder)
+            val_img_folder = os.path.join(self.data_path, 'val/images')
+            val_mask_folder = os.path.join(self.data_path, 'val/labels')
+            val_img_paths, val_mask_paths = self.__get_pairs(val_img_folder, val_mask_folder)
+            self.images = train_img_paths + val_img_paths
+            self.mass = train_mask_paths + val_mask_paths
 
-        random.shuffle(datalist)
-        for idx, line in enumerate(datalist):
-            line = line.strip().split(' ')
-            path = os.path.join(self.data_base_dir, line[0])
-            label = line[-1]
-            self.data.append((path, int(label)))
-
+        if len(self.images) != len(self.masks):
+            raise RuntimeError("the number of images and masks does not matching")
 
     def __len__(self):
-        return len(self.data)
-
+        return len(self.images)
 
     def __getitem__(self, index):
-        path, label = self.data[index]
 
-        img = Image.open(path)
+        img = imageio.imread(self.images[index])
+        mask = imageio.imread(self.masks[index])
 
-        img_ndarr = np.asarray(img)
-        if img_ndarr.shape[2] != 3:
-            img_ndarr = img_ndarr[:,:,0:3]
-            img = Image.fromarray(img_ndarr)
+        # random left-right flip
+        if random.random() < 0.5:
+            img = iaa.Fliplr(1.0).augment_images(img)
+            mask = iaa.Fliplr(1.0).augment_images(mask)
+        # random up-down flip
+        if random.random() < 0.5:
+            img = iaa.Flipud(1.0).augment_image(img)
+            mask = iaa.Flipud(1.0).augment_image(mask)
 
-        if img.mode == 'L':
-            img = img.convert('RBG')
 
-        # img = scale_byRatio(path, return_width=IMSIZE)
-        # img = img / 255.
 
-        if self._transform:
-            return self.transform(img, label)
-
-        return img, label
+        return img, mask
 
 
     def transform(self, img, lbl):
