d71a5bdbda688eb0336e7d9876ea05fd3ed6ca7b
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-03-27 18:05:44 +0800

    Update meter.py

--- utils/meter.py
+++ utils/meter.py
@@ -11,7 +11,6 @@ class SegmentationErrorMeter(object):
         self.total_union = 0
         self.nbr_classes = nbr_classes
 
-
     def reset(self):
         self.total_correct = 0
         self.total_labeled = 0
@@ -20,7 +19,7 @@ class SegmentationErrorMeter(object):
 
     def add(self, output, target):
         if torch.is_tensor(output):
-            output = output.cpu().squeeze().numpy().astype('int32') + 1
+            output = output.cpu().squeeze().numpy()
         if torch.is_tensor(target):
             target = target.cpu().squeeze().numpy().astype('int32') + 1
         correct, labeled = batch_pixel_accuracy(output, target)
@@ -28,11 +27,16 @@ class SegmentationErrorMeter(object):
         self.total_labeled += target
 
 def batch_pixel_accuracy(output, target):
-    output = np.argmax(output, axis=1)
+    output = np.argmax(output, axis=1) + 1
     pixel_labeled = np.sum(target > 0)
     pixel_correct = np.sum((output == target) * (target > 0))
     return pixel_correct, pixel_labeled
 
 def batch_intersection_union(output, target, nbr_classes):
-    output = np.argmax(output, axis=1)
+    output = np.argmax(output, axis=1) + 1
+    output = output * (target > 0).astype('int32')
+    intersection = output * (output == target)
+    area_inter, _ = np.histogram(intersection, bins=nbr_classes, range=(1, nbr_classes))
+    area_pred, _ = np.histogram(output, bins=nbr_classes, range=(1, nbr_classes))
+    area_lab, _ = np.histogram(target, bins=nbr_classes, range=(1, nbr_classes))
 
