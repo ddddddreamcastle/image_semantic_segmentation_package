e59811830d3f9ef518cfca14d1f8acd74ae6f540
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-03-28 11:05:20 +0800

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,13 +1,17 @@
-from .ade20k import train_loader_ade20k, val_loader_ade20k
+import torch.utils.data as data
+from .ade20k import ADE20K
 
-def get_train_val_dataset(name, kwargs):
-    """
-    return a tuple (train_loader, val_loader)
-    :param name: data set name
-    :param kwargs: other params needed by data loader
-    :return: tuple
-    """
-    dataset = {
-        'ade20k': (train_loader_ade20k, val_loader_ade20k)
+datasets = {
+        'ade20k': ADE20K
     }
-    return dataset[name][0](**kwargs), dataset[name][1](**kwargs)
+
+def get_train_val_loader(name, image_size=384, data_path='./data/', batch_size=8, shuffle=True, num_workers=4,
+                         pin_memory=False, **kwargs):
+    global datasets
+    train_loader = data.DataLoader(datasets[name](mode='train', image_size=image_size, data_path=data_path),
+                                   batch_size=batch_size, shuffle=shuffle, num_workers=num_workers,
+                                   pin_memory=pin_memory)
+    val_loader = data.DataLoader(datasets[name](mode='val', image_size=image_size, data_path=data_path),
+                                 batch_size=batch_size, shuffle=shuffle, num_workers=num_workers,
+                                 pin_memory=pin_memory)
+    return train_loader, val_loader
