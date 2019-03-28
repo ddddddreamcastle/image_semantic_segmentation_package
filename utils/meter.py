b60fc2a9df732b98f8069d66be38abcd4824bb23
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-03-28 09:20:33 +0800

    Update meter.py

--- utils/meter.py
+++ utils/meter.py
@@ -4,6 +4,12 @@ import numpy as np
 class SegmentationErrorMeter(object):
 
     def __init__(self, metrics, nbr_classes):
+        self.metrics_library = {'pixAcc': (self.__batch_pixel_accuracy, self.__pixAcc),
+                                'mIoU': (self.__batch_intersection_union, self.__mIoU)}
+        for m in metrics:
+            if m not in self.metrics_library:
+                raise RuntimeError("metrics can only be choose from `pixAcc`, `mIoU` ")
+
         self.metrics = metrics
         self.total_correct = 0
         self.total_label = 0
@@ -22,31 +28,35 @@ class SegmentationErrorMeter(object):
             output = output.cpu().squeeze().numpy()
         if torch.is_tensor(target):
             target = target.cpu().squeeze().numpy().astype('int32') + 1
-        correct, labeled = batch_pixel_accuracy(output, target)
-        inter, union = batch_intersection_union(output, target, self.nbr_classes)
-        self.total_correct += correct
-        self.total_labeled += target
-        self.total_inter += inter
-        self.total_union += union
+        for m in self.metrics:
+            self.metrics_library[m][0](output, target)
 
     def value(self):
-        pixAcc = 1.0 * self.total_correct / (np.spacing(1) + self.total_label)
-        IoU = 1.0 * self.total_inter / (np.spacing(1) + self.total_union)
-        mIoU = IoU.mean()
-        return pixAcc, mIoU
+        return_value = []
+        for m in self.metrics:
+            return_value.append(self.metrics_library[m][1]())
+        return tuple(return_value)
 
-def batch_pixel_accuracy(output, target):
-    output = np.argmax(output, axis=1) + 1
-    pixel_labeled = np.sum(target > 0)
-    pixel_correct = np.sum((output == target) * (target > 0))
-    return pixel_correct, pixel_labeled
+    def __batch_pixel_accuracy(self, output, target):
+        output = np.argmax(output, axis=1) + 1
+        pixel_labeled = np.sum(target > 0)
+        pixel_correct = np.sum((output == target) * (target > 0))
+        self.total_correct += pixel_correct
+        self.total_labeled += pixel_labeled
 
-def batch_intersection_union(output, target, nbr_classes):
-    output = np.argmax(output, axis=1) + 1
-    output = output * (target > 0).astype('int32')
-    intersection = output * (output == target)
-    area_inter, _ = np.histogram(intersection, bins=nbr_classes, range=(1, nbr_classes))
-    area_pred, _ = np.histogram(output, bins=nbr_classes, range=(1, nbr_classes))
-    area_label, _ = np.histogram(target, bins=nbr_classes, range=(1, nbr_classes))
-    area_union = area_pred + area_label - area_inter
-    return area_inter, area_union
+    def __pixAcc(self):
+        return 1.0 * self.total_correct / (np.spacing(1) + self.total_label)
+
+    def __batch_intersection_union(self, output, target):
+        output = np.argmax(output, axis=1) + 1
+        output = output * (target > 0).astype('int32')
+        intersection = output * (output == target)
+        area_inter, _ = np.histogram(intersection, bins=self.nbr_classes, range=(1, self.nbr_classes))
+        area_pred, _ = np.histogram(output, bins=self.nbr_classes, range=(1, self.nbr_classes))
+        area_label, _ = np.histogram(target, bins=self.nbr_classes, range=(1, self.nbr_classes))
+        area_union = area_pred + area_label - area_inter
+        self.total_inter += area_inter
+        self.total_union += area_union
+
+    def __mIoU(self):
+        return (1.0 * self.total_inter / (np.spacing(1) + self.total_union)).mean()
