8af6eae66682d333ba300ef44aa6ae1241112bac
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-03-29 17:19:25 +0800

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -5,13 +5,13 @@ datasets = {
         'ade20k': ADE20K
     }
 
-def get_train_val_loader(name, image_size=384, data_path='./data/', batch_size=8, shuffle=True, num_workers=4,
+def get_train_val_loader(name, image_size=384, data_path='./data/', batch_size=8, shuffle=True, dataloader_workers=4,
                          pin_memory=False, **kwargs):
     global datasets
     train_loader = data.DataLoader(datasets[name](mode='train', image_size=image_size, data_path=data_path),
-                                   batch_size=batch_size, shuffle=shuffle, num_workers=num_workers,
+                                   batch_size=batch_size, shuffle=shuffle, num_workers=dataloader_workers,
                                    pin_memory=pin_memory)
     val_loader = data.DataLoader(datasets[name](mode='val', image_size=image_size, data_path=data_path),
-                                 batch_size=batch_size, shuffle=shuffle, num_workers=num_workers,
+                                 batch_size=batch_size, shuffle=shuffle, num_workers=dataloader_workers,
                                  pin_memory=pin_memory)
     return train_loader, val_loader
