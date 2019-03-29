f57825a03e5b51a71a8c3a3a53f869da03179b71
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-03-29 15:54:59 +0800

    Update model_manager.py

--- models/model_manager.py
+++ models/model_manager.py
@@ -4,6 +4,7 @@ import torch
 from loss import SegmentationLoss
 from torch import nn
 from utils.learning_rate_scheduler import LearningRateScheduler
+from utils.learning_rate_scheduler import lr_parse
 from tqdm import tqdm
 from utils.meter import SegmentationErrorMeter
 from utils.recorder import save_checkpoint
@@ -15,21 +16,22 @@ from utils.color import add_color
 class Manager(object):
 
     def __init__(self, args):
+        args = lr_parse(args)
         self.kwargs = vars(args)
         self.epochs = args.epochs
         self.model = get_model(name=args.model, kwargs=self.kwargs)
         self.train_loader, self.val_loader = get_train_val_loader(args.dataset, **self.kwargs)
 
-        parameters = self.model.get_parameters_as_groups(args.lr)
+        parameters = self.model.get_parameters_as_groups(args.learning_rate)
 
-        self.optimizer = torch.optim.Adam(parameters, lr=args.lr,
-                                          weight_decay=args.weight_decay)
+        self.optimizer = torch.optim.SGD(parameters, lr=args.learning_rate,
+                                          weight_decay=args.weight_decay, momentum=args.momentum)
 
         if args.supervision:
-            self.criterion = SegmentationLoss((nn.CrossEntropyLoss, nn.CrossEntropyLoss),
+            self.criterion = SegmentationLoss((nn.CrossEntropyLoss(), nn.CrossEntropyLoss()),
                                      (1, args.supervision_weight))
         else:
-            self.criterion = SegmentationLoss((nn.CrossEntropyLoss, ),
+            self.criterion = SegmentationLoss((nn.CrossEntropyLoss(), ),
                                          (1, ))
 
         if torch.cuda.is_available():
@@ -60,12 +62,15 @@ class Manager(object):
                 image = image.cuda()
                 target = target.cuda()
             preds = self.model(image)
-            meter.add(preds[0], target)
-            pixAcc, mIoU = meter.values()
             loss = self.criterion(preds, target)
             loss.backward()
             self.optimizer.step()
             train_loss += loss.item()
+            pred = preds
+            if self.model.deep_supervision:
+                pred = preds[0]
+            meter.add(pred, target)
+            pixAcc, mIoU = meter.values()
             tqdm_bar.set_description('Lr: {:.4}, Train loss: {:.3}, Train pixAcc: {:.3}, Train mIoU: {:.3}'
                                      .format(cur_lr, train_loss/(i+1), pixAcc, mIoU))
 
@@ -77,7 +82,10 @@ class Manager(object):
             if torch.cuda.is_available():
                 image = image.cuda()
                 target = target.cuda()
-            pred = self.model(image)[0]
+            preds = self.model(image)
+            pred = preds
+            if self.model.deep_supervision:
+                pred = preds[0]
             meter.add(pred, target)
             pixAcc, mIoU = meter.values()
             tqdm_bar.set_description('pixAcc: {:.3}, mIoU: {:.3}'.format(pixAcc, mIoU))
@@ -90,7 +98,7 @@ class Manager(object):
                         best_performance=self.best_performance, is_best=is_best, **self.kwargs)
 
     def fit(self):
-        for epoch in self.epochs:
+        for epoch in range(self.epochs):
             print('Epoch : {}'.format(epoch + 1))
             # train step
             self.model.train()
@@ -126,7 +134,7 @@ class Manager(object):
             if self.model.deep_supervision:
                 pred = pred[0]
             img = color(ori, pred)
-            filename = os.path.splitext(filepath[filepath.rfind('/'):])
+            filename, _ = os.path.splitext(filepath[filepath.rfind('/'):])
             Image.fromarray(img).save(os.path.join(output_path, "result_{}.jpg".format(filename)))
 
         self.model.eval()
