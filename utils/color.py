2fe4cb87fd8b726e34b40840afa8d4d2f483721a
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-03-29 11:20:51 +0800

    Update color.py

--- utils/color.py
+++ utils/color.py
@@ -14,14 +14,13 @@ def class2color_idx(c, x):
 def add_color(cm, nbr_classes):
     h, w = cm.shape
     img_color = np.zeros((h, w, 3))
-    for i in range(nbr_classes):
+    for i in range(0, nbr_classes):
         img_color[cm == i] = to_color(i, nbr_classes)
-
-
-
+    img_color[cm == 0] = (1.0, 1.0, 1.0)
+    return img_color
 
 def to_color(category, nbr_classes):
-    colormap = np.linspace(0.1, 1, math.ceil(nbr_classes ** (1 / 3)))
+    colormap = np.linspace(0, 1, math.ceil(nbr_classes ** (1 / 3)))
     color_index = class2color_idx(category, len(colormap))
-    r, g, b = int(colormap[color_index[0]]*255), int(colormap[color_index[1]]*255), int(colormap[color_index[2]]*255)
+    r, g, b = colormap[color_index[0]], colormap[color_index[1]], colormap[color_index[2]]
     return  r, g, b
