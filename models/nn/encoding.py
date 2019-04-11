3f484dd2eb9fa30ae317a51cd1269d942e36642c
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-04-11 16:44:45 +0800

    update

--- models/nn/encoding.py
+++ models/nn/encoding.py
@@ -3,7 +3,7 @@ from torch import nn
 import torch.nn.functional as F
 from torch.autograd import Function
 
-class ScaledL2(Function):
+class scaled_L2(Function):
 
     @staticmethod
     def forward(ctx, X, C, S):
@@ -13,5 +13,27 @@ class ScaledL2(Function):
         return SL
 
     @staticmethod
-    def backward(ctx, grad_outputs):
+    def backward(ctx, grad_SL):
         X, C, S, SL = ctx.saved_variables
+        tmp = (2 * grad_SL * S.view(1, 1, C.size(0))).unsqueeze(3) * \
+                (X.unsqueeze(2).expand(X.size(0), X.size(1), C.size(0), C.size(1)) - C.unsqueeze(0).unsqueeze(0))
+        GX = tmp.sum(2)
+        GC = tmp.sum(0).sum(0)
+        GS = (grad_SL * (SL / S.view(1, 1, C.size(0)))).sum(0).sum(0)
+        return GX, GC, GS
+
+class aggregate(Function):
+
+    @staticmethod
+    def forward(ctx, A, X, C):
+        ctx.save_for_backward(A, X, C)
+        E = (A.unsqueeze(3) * (X.unsqueeze(2).expand(X.size(0), X.size(1),
+                                                      C.size(0), C.size(1)) - C.unsqueeze(0).unsqueeze(0))).sum(1)
+        return E
+
+    @staticmethod
+    def backward(ctx, grad_E):
+
+
+
+
