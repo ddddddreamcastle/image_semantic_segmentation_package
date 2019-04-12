b0e3a140be1cf280a29f6fca4aec79c43b189de2
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-04-12 17:28:54 +0800

    update

--- models/nn/encnet.py
+++ models/nn/encnet.py
@@ -1,20 +1,35 @@
 import torch.nn as nn
 from datasets import datasets
 import torch
+from backbone import get_backbone
 from .encoding import scaled_l2, aggregate
 
 class EncCore(nn.Module):
-    def __init__(self, in_channels, out_channels, up_method):
+    def __init__(self, in_channels, out_channels, up_method, se_loss):
         super(EncCore, self).__init__()
+        self.top = nn.Sequential(
+            nn.Conv2d(in_channels, 512, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True)
+        )
+        self.encoding = nn.Sequential(
+            nn.Conv2d(512, 512, 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(512),
 
-    def forward(self, *input):
+        )
+
+
+    def forward(self, x):
         pass
 
 class EncNet(nn.Module):
-    def __init__(self, nbr_classes, deep_supervision=True, backbone='resnet50', **kwargs):
+    def __init__(self, nbr_classes, deep_supervision=True, backbone='resnet50', se_loss=True, **kwargs):
         super(EncNet, self).__init__()
+        self.backbone = get_backbone(backbone, backbone_pretrained=True, **kwargs)
+        self.core = EncCore(in_channels=2048, out_channels=nbr_classes, up_method=self.up_method, se_loss=se_loss)
 
-    def forward(self, *input):
+    def forward(self, x):
         pass
 
 def get_encnet(backbone='resnet50', model_pretrained=True, supervision=True,
