f8233f0e28ed8cb4fdf18fb1499366ed1bb351d0
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-04-29 14:22:32 +0800

    add xception

--- backbone/__init__.py
+++ backbone/__init__.py
@@ -1,6 +1,9 @@
 from .resnet import get_resnet
+from .xception import get_xception
+
 def get_backbone(name, **kwargs):
     models = {
-        'resnet50':  get_resnet(50)
+        'resnet50':  get_resnet(50),
+        'xception': get_xception()
     }
     return models[name](**kwargs)
