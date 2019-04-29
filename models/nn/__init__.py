88bfd4cef2fad1b38082d416c440d96ef064a693
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-04-29 14:24:35 +0800

    add deeplabv3 option

--- models/nn/__init__.py
+++ models/nn/__init__.py
@@ -1,8 +1,10 @@
 from .pspnet import get_pspnet
 from .encnet import get_encnet
+from .deeplabv3 import get_deeplabv3
 def get_model(name, kwargs):
     models = {
         'pspnet': get_pspnet,
-        'encnet': get_encnet
+        'encnet': get_encnet,
+        'deeplabv3': get_deeplabv3
     }
     return models[name](**kwargs)
