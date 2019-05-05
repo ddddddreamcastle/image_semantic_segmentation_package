0cad2ab5e21b5db8f80925f551205a9442681de6
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-05-05 11:37:49 +0800

    update

--- backbone/xception.py
+++ backbone/xception.py
@@ -12,20 +12,21 @@ from torchviz import make_dot
 
 class SeparableConv2d(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False,
-                 depth_activation=True, inplace=True):
+                 depth_activation=False, inplace=True):
         super(SeparableConv2d, self).__init__()
         self.relu_0 = nn.ReLU(inplace)
 
         self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size, stride=stride, padding=padding,
                                    dilation=dilation, bias=bias, groups=in_channels)
         self.bn_1 = nn.BatchNorm2d(in_channels)
-        self.relu_1 = nn.ReLU(inplace)
+        self.relu_1 = nn.ReLU(True)
 
         self.pointwise = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=bias)
         self.bn_2 = nn.BatchNorm2d(out_channels)
-        self.relu_2 = nn.ReLU(inplace)
+        self.relu_2 = nn.ReLU(True)
 
         self.depth_activation = depth_activation
+
 
     def forward(self, x):
         if not self.depth_activation:
@@ -41,12 +42,15 @@ class SeparableConv2d(nn.Module):
         return x
 
 class Block(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, dilation=1, depth_activation=True, grow_first=True):
+    def __init__(self, in_channels, out_channels, stride=1, dilation=1, depth_activation=False, grow_first=True,
+                 inplace=True):
         super(Block, self).__init__()
 
+        head_relu = True
         if in_channels != out_channels or stride != 1:
             self.skip = nn.Conv2d(in_channels, out_channels, 1, stride=stride, bias=False)
             self.skip_bn = nn.BatchNorm2d(out_channels)
+            head_relu = False
         else:
             self.skip = None
 
@@ -56,11 +60,13 @@ class Block(nn.Module):
             inner_channels = in_channels
 
         self.layer_1 = SeparableConv2d(in_channels, inner_channels, 3, stride=1, padding=dilation, dilation=dilation,
-                                       bias=False, depth_activation=depth_activation)
+                                       bias=False, depth_activation=depth_activation, inplace=head_relu)
         self.layer_2 = SeparableConv2d(inner_channels, out_channels, 3, stride=1, padding=dilation, dilation=dilation,
                                        bias=False, depth_activation=depth_activation)
         self.layer_3 = SeparableConv2d(out_channels,out_channels,3,stride=stride, padding=dilation,dilation=dilation,
-                                        bias=False,depth_activation=depth_activation, inplace=False)
+                                        bias=False,depth_activation=depth_activation, inplace=inplace)
+
+        self.outer_branch = None
 
     def forward(self, x):
         skip = x
@@ -69,6 +75,7 @@ class Block(nn.Module):
             skip = self.skip_bn(skip)
         main = self.layer_1(x)
         main = self.layer_2(main)
+        self.outer_branch = main
         main = self.layer_3(main)
 
         main += skip
@@ -91,7 +98,7 @@ class Xception(nn.Module):
         self.bn_2 = nn.BatchNorm2d(64)
 
         self.block_1 = Block(64, 128, stride=2)
-        self.block_2 = Block(128, 256, stride=strides[0])
+        self.block_2 = Block(128, 256, stride=strides[0], inplace=False)
         self.block_3 = Block(256, 728, stride=strides[1])
 
         rate = 16 // os
@@ -119,6 +126,7 @@ class Xception(nn.Module):
                 layer.bias.data.zero_()
 
     def base_forward(self, x):
+        self.outer_branches = []
         x = self.conv_1(x)
         x = self.bn_1(x)
         x = self.relu(x)
@@ -129,6 +137,7 @@ class Xception(nn.Module):
 
         x = self.block_1(x)
         x = self.block_2(x)
+        self.outer_branches.append(self.block_2.outer_branch)
         x = self.block_3(x)
 
         for block in self.blocks:
