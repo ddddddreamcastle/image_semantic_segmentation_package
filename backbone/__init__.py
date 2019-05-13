143cb3264267760baf3d49354780252b70c95e2c
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-05-13 17:07:42 +0800

    add some options

--- backbone/__init__.py
+++ backbone/__init__.py
@@ -1,9 +1,20 @@
 from .resnet import get_resnet
 from .xception import get_xception
+from .vgg import get_vgg
 
 def get_backbone(name, **kwargs):
     models = {
         'resnet50':  get_resnet(50),
-        'xception': get_xception()
+        'resnet101': get_resnet(101),
+        'resnet152': get_resnet(152),
+        'xception': get_xception(),
+        'vgg11': get_vgg(11, False),
+        'vgg13': get_vgg(13, False),
+        'vgg16': get_vgg(16, False),
+        'vgg19': get_vgg(19, False),
+        'vgg11bn': get_vgg(11, True),
+        'vgg13bn': get_vgg(13, True),
+        'vgg16bn': get_vgg(16, True),
+        'vgg19bn': get_vgg(19, True)
     }
     return models[name](**kwargs)
