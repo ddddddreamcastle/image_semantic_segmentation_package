bd8e937bb6c9fdda7175f7a434ccb87286ab7e88
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-05-20 21:00:46 +0800

    update

--- backbone/vgg.py
+++ backbone/vgg.py
@@ -4,10 +4,13 @@ import torch
 from models.components.norm import get_norm
 
 cfg = {
-    11: [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-    13: [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
     16: [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
     19: [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+conn_cfg = {
+    16: {True: [5, 12, 22, 32, 42], False: [3, 8, 15, 22, 29]},
+    19: {True: [5, 12, 25, 38, 51], False: [3, 8, 17, 26, 35]}
 }
 
 class VGG(nn.Module):
@@ -23,12 +26,26 @@ class VGG(nn.Module):
             nn.Dropout(),
             nn.Linear(4096, nbr_classes)
         )
+        self.nbr_layers = nbr_layers
+        self.batch_norm = batch_norm
+
+    def base_forward(self, x):
+        self.skip_connections = []
+        for idx, l in enumerate(self.features):
+            x = l(x)
+            if idx in conn_cfg[self.nbr_layers][self.batch_norm]:
+                self.skip_connections.append(x)
+        self.skip_connections.reverse()
+        return x
 
     def forward(self, x):
-        x = self.features(x)
+        x = self.base_forward(x)
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return x
+
+    def backbone_forward(self, x):
+        return self.base_forward(x)
 
     def _make_layers(self, cfg, batch_norm=True, dilation=True, norm='bn'):
         layers = []
@@ -53,7 +70,7 @@ class VGG(nn.Module):
         return nn.Sequential(*layers)
 
 def get_vgg(nbr_layers=16, batch_norm=True, dilation=False):
-    def build_net(backbone_pretrained_path='./weights/vgg16.pth', nbr_classes=1000,
+    def build_net(backbone_pretrained_path='./weights/vgg19.pth', nbr_classes=1000,
                   backbone_pretrained=True, norm='bn', **kwargs):
         model = VGG(nbr_classes, nbr_layers=nbr_layers, batch_norm=batch_norm, dilation=dilation, norm=norm)
         if backbone_pretrained:
@@ -63,16 +80,7 @@ def get_vgg(nbr_layers=16, batch_norm=True, dilation=False):
     return build_net
 
 if __name__ == '__main__':
-    vgg = VGG(nbr_classes=1000, nbr_layers=16, batch_norm=True, dilation=False)
-    g = make_dot(vgg(torch.rand(16, 3, 224, 224)), params=dict(vgg.named_parameters()))
-    g.render('vgg16')
-    params = list(vgg.parameters())
-    k = 0
-    for i in params:
-        l = 1
-        print("layer architecture：" + str(list(i.size())))
-        for j in i.size():
-            l *= j
-        print("the number of parameters：" + str(l))
-        k = k + l
-    print("the total of parameters：" + str(k))
+    vgg = VGG(nbr_classes=1000, nbr_layers=19, batch_norm=False, dilation=False, norm='bn')
+    for idx, v in enumerate(list(vgg.children())):
+        print(idx, v)
+    # g = make_dot(vgg(torch.rand(16, 3, 256, 256)), params=dict(vgg.named_parameters()))
