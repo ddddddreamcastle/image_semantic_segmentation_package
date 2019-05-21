685d82259c13a4d654b6548a36800e4a33b002c0
ddddddreamcastle <45117171+ddddddreamcastle@users.noreply.github.com>
2019-05-21 09:22:49 +0800

    update

--- backbone/__init__.py
+++ backbone/__init__.py
@@ -8,12 +8,8 @@ def get_backbone(name, **kwargs):
         'resnet101': get_resnet(101),
         'resnet152': get_resnet(152),
         'xception': get_xception(),
-        'vgg11': get_vgg(11, False),
-        'vgg13': get_vgg(13, False),
         'vgg16': get_vgg(16, False),
         'vgg19': get_vgg(19, False),
-        'vgg11bn': get_vgg(11, True),
-        'vgg13bn': get_vgg(13, True),
         'vgg16bn': get_vgg(16, True),
         'vgg19bn': get_vgg(19, True)
     }
